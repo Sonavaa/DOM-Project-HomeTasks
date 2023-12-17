b5944076aafd664f16fce1b7b9bb6247857f3840
Sonava <sonava@code.edu.az>
2023-12-17 04:16:57 +0400

    Slider Done

--- DOM Projects/Slider/script.js
+++ DOM Projects/Slider/script.js
@@ -6,16 +6,7 @@ const slider = document.querySelector(".slider-image");
 
 const images = ["beach", "building", "city", "mozaic", "sky"];
 
-let index = 0;
-
-left_btn.addEventListener("click", () => {
-    if(index > 0){
-        index--;
-    }
-    else{
-        index = images.length-1
-    }
-});
+let index = 1;
 
 right_btn.addEventListener("click", () => {
     if(index < images.length-1){
@@ -24,12 +15,45 @@ right_btn.addEventListener("click", () => {
     else{
         index = 0
     }
+    showImage();
 });
-console.log(index)
 
+//On Keyboard, RightArrow Works right
+window.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowRight") {
+        if(index < images.length-1){
+            index++;
+        }
+        else{
+            index = 0
+        }
+        showImage();
+    }
+  });
 
-// const showImage = () => {
-//     slider.style.backgroundImage = `url("./assets/images/${images[index]}.jpg")`;
-// }
+left_btn.addEventListener("click", () => {
+    if(index > 0){
+        index--;
+    }
+    else{
+        index = images.length-1
+    }
+    showImage();
+});
 
-// showImage();
+//On Keyboard, LeftArrow Works right
+window.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        if(index > 0){
+            index--;
+        }
+        else{
+            index = images.length-1
+        }
+        showImage();
+    }
+  });
+
+const showImage = () => {
+    slider.style.backgroundImage = `url("./assets/images/${images[index]}.jpg")`;
+}
