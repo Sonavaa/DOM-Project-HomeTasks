10e022a8778e8cd569c78bbb00ec623e37e71af4
Sonava <sonava@code.edu.az>
2023-12-17 04:37:18 +0400

    Keyboard Events Done

--- DOM Projects/Keyboard Event/script.js
+++ DOM Projects/Keyboard Event/script.js
@@ -1,1 +1,22 @@
+"use strict";
 
+const insert_el = document.getElementById("insert"); 
+const key = document.getElementsByClassName("key");
+
+window.addEventListener("keydown", function(event) {
+    insert_el.innerHTML = `
+    <div class="key">
+    <small>Key</small>
+    <p>${event.key}</p>
+    </div>
+    
+    <div class="key">
+    <small>KeyCode</small>
+    <p>${event.keyCode}</p>
+    </div>
+    
+    <div class="key">
+    <small>Code</small>
+    <p>${event.code}</p>
+    </div>`
+});
