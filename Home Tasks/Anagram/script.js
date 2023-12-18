f61f920637b0abad50470b1b17647292a394d91e
Sonava <sonava@code.edu.az>
2023-12-18 12:33:57 +0400

    Anagram done, but array sort method is not working

--- Home Tasks/Anagram/script.js
+++ Home Tasks/Anagram/script.js
@@ -1,15 +1,17 @@
 "use strict";
-let string_1 = prompt("Please enter a string");
-let string_2 = prompt("Please enter a string");
+const string_1 = prompt("Please enter a string");
+const string_2 = prompt("Please enter a string");
 
 function checkAnagram(string_1, string_2) {
 
-    let str_1=string_1.toLowerCase().split(" ").sort().join(" ")
-    let str_2=string_2.toLowerCase().split(" ").sort().join(" ")
+    const str_1 = string_1.toLowerCase().split(" ").sort().join(" ")
+    console.log(str_1)
+    const str_2 = string_2.toLowerCase().split(" ").sort().join(" ")
+    console.log(str_2)
     if (str_1 !== str_2) {
-       console.log(`${string_1} and ${string_2} are not anagrams`);
+       console.log(`${str_1} and ${str_2} are not anagrams`);
     } else {
-        console.log(`${string_1} and ${string_2} are anagrams`);
+        console.log(`${str_1} and ${str_2} are anagrams`);
     }
 }
 
